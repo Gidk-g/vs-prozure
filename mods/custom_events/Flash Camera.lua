1f3aadfb9f15f7223323942846e573c5abc73442
Gidk-g <101887005+Gidk-g@users.noreply.github.com>
2023-04-30 15:09:28 +0200

    Update Flash Camera.lua

--- mods/custom_events/Flash Camera.lua
+++ mods/custom_events/Flash Camera.lua
@@ -1,13 +1,13 @@
 function onEvent(name, value1)
 	if name == 'Flash Camera' then
 	    makeLuaSprite('flash', '', 0, 0);
-        makeGraphic('flash', 1280, 720, 'ffffff')
+            makeGraphic('flash', 1280, 720, 'ffffff')
 	    addLuaSprite('flash', true);
 	    setLuaSpriteScrollFactor('flash', 0, 0)
 	    setProperty('flash.scale.x', 2)
 	    setProperty('flash.scale.y', 2)
 	    setProperty('flash.alpha', 0)
-		setProperty('flash.alpha', 1)
-		doTweenAlpha('flash', 'flash', 0, value1, 'linear')
+	    setProperty('flash.alpha', 1)
+	    doTweenAlpha('flash', 'flash', 0, value1, 'linear')
 	end
 end
