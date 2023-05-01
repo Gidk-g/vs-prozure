3671487d6818618d80e49a676f406ab40e1d79b5
Gidk-g <gidkg302@gmail.com>
2023-05-01 17:50:40 +0200

    Update Cam Boom Speed.lua

--- mods/custom_events/Cam Boom Speed.lua
+++ mods/custom_events/Cam Boom Speed.lua
@@ -2,13 +2,14 @@ boomspeed = 4
 bam = 1
 songEnded = false
 
-function onEvent(n,v1,v2)
-    if n == "Cam Boom Speed" then
-        boomspeed = tonumber(v1)
-        bam = tonumber(v2)
+function onEvent(name, value1, value2)
+    if name == "Cam Boom Speed" then
+        boomspeed = tonumber(value1)
+        bam = tonumber(value2)
     end
 end
-function onBeatHit()
+
+function onBeatHit(curBeat)
 	if curBeat % boomspeed == 0 and not songEnded then
 		triggerEvent("Add Camera Zoom", 0.015 * bam, 0.03 * bam)
 		if getProperty('camGame.zoom') >= 1.35 then
